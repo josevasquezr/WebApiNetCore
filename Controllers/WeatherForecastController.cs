76f738f22e549f3729b819f7c2dbceb4385b98fe
jvasquez <javierv.ramos@gmail.com>
2022-05-26 20:41:29 -0600

    Creacion de diversas rutas para Verbos HTTP

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -12,21 +12,49 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private static List<WeatherForecast> WeatherForecasts { get; set; } = new List<WeatherForecast>();
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (WeatherForecasts == null || !WeatherForecasts.Any())
+        {
+            WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Route("[action]")]
+    [Route("GetListWeather")]
+    [Route("GetListWeatherV2")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return WeatherForecasts;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast){
+        WeatherForecasts.Add(weatherForecast);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id){
+        try
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            WeatherForecasts.RemoveAt(id);
+            return Ok();
+        }
+        catch (System.Exception ex)
+        {
+            return NotFound($"No se encontro resgistro con el id: {id}");
+        }
     }
 }
