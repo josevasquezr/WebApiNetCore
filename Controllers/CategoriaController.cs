c67083c4973a5f79af8d7c9b976fd45d046c7a14
jvasquez <javierv.ramos@gmail.com>
2022-05-29 16:59:31 -0600

    Creacion de controladores por medio de servicios usando inyeccion de dependencias

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        ICategoriasService categoriasService;
+
+        public CategoriaController(ICategoriasService service)
+        {
+            categoriasService = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(categoriasService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Categoria categoria)
+        {
+            categoriasService.Save(categoria);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Categoria categoria)
+        {
+            categoriasService.Update(id, categoria);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            categoriasService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/TareaController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TareaController : ControllerBase
+    {
+        ITareasService tareaService;
+
+        public TareaController(ITareasService service)
+        {
+            tareaService = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(tareaService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Tarea tarea)
+        {
+            tareaService.Save(tarea);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Tarea tarea)
+        {
+            tareaService.Update(id, tarea);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            tareaService.Delete(id);
+            return Ok();
+        }
+    }
+}
